2ec927ee8043bd1aed07c0cf70fc165d8a04403f
Ritesh <riteshkumar05surat@gmail.com>
2024-08-19 12:04:23 +0530

    fixed problem

--- index.js
+++ index.js
@@ -16,35 +16,43 @@ btn.addEventListener("click", (e) => {
     result.innerHTML = "<h2 class='text-center'>Wait a moment !!</h2>";
     // console.log(input.value);
     const url = `https://newsapi.org/v2/top-headlines?q=${input.value}&apiKey=${API_KEY}`;
-    dataJson = fetchData(url);
-    dataJson.then(data => {
-        if (data["status"] = "ok") {
-            if (data["totalResults"] > 0) {
-                articles = data["articles"]
-                result.innerHTML = "";
-                Array.from(articles).forEach(element => {
-                    // console.log(element)
-                    let index = element["content"].indexOf("[+");
+    if (input.value.length > 1) {
+        dataJson = fetchData(url);
+        dataJson.then(data => {
+            if (data["status"] = "ok") {
+                if (data["totalResults"] > 0) {
+                    articles = data["articles"]
+                    result.innerHTML = "";
+                    Array.from(articles).forEach(element => {
+                        console.log(element)
+                        if (String(element["content"]).length > 10) {
+                            let index = element["content"].indexOf("[");
+                            result.innerHTML += `
+                            <div class="cards">           
+                                <h3 class="title">${element["title"]}</h3>
+                                <p class="content">${element["content"].slice(0, index)}</p>
+                                <p class="author"><b>Author: </b>${element["author"]}</p>
+                                <div class="readBtn"><a href="${element["url"]}">Read</a></div>                           
+                            </div>`;
+                        }
 
-                    result.innerHTML += `
-                        <div class="cards">           
-                            <h3 class="title">${element["title"]}</h3>
-                            <p class="content">${element["content"].slice(0, index)}</p>
-                            <p class="author"><b>Author: </b>${element["author"]}</p>
-                            <div class="readBtn"><a href="${element["url"]}">Read</a></div>                           
-                        </div>`;
-                });
-                input.value="";
+                    });
+                    input.value = "";
+                }
+                else {
+                    console.log("No data found");
+                    result.innerHTML = "<h2 class='text-center'>No data found !!</h2"
+                }
             }
             else {
-                console.log("No data found");
-                result.innerHTML = "<h2 class='text-center'>No data found !!</h2"
+                console.log("error occured");
+                result.innerHTML = "<h2 class='text-center'>Error Occured !!</h2"
             }
-        }
-        else {
-            console.log("error occured");
-            result.innerHTML = "<h2 class='text-center'>Error Occured !!</h2"
-        }
 
-    })
+        })
+    }
+    else {
+        console.log("Please Give Some Argument");
+        result.innerHTML = "<h2 class='text-center'>Please Give Some Argument</h2"
+    }
 });
